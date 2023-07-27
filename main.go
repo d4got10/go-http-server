486060381cfd3ea0115288747e52cb5ed14ca817
d4got10 <rusbeks@outlook.com>
2023-07-27 14:29:12 +1000

    Parse headers from request

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -24,23 +26,25 @@ func start_server(address string) {
         return
     }
 
-    fmt.Printf("Server started!\n")
+    fmt.Printf("Server listening on %s!\n", listener.Addr().String())
 
     for {
-        fmt.Printf("Waiting for connection from client...\n")
+        //fmt.Printf("Waiting for connection from client...\n")
         conn, err := listener.Accept()
         if err != nil {
             fmt.Printf("Error accepting new connection: %s\n", err.Error())
             return
         }
 
-        fmt.Printf("Client connected!\n")
+        //fmt.Printf("Client connected!\n")
 
-        err = receive_data(conn)
+        headers, err := receive_data(conn)
         if err != nil {
             fmt.Printf("Error receiving data from client: %s", err.Error())
             continue
         }
+        fmt.Printf("%s\n", headers["Status"])
+
         err = send_data(conn)
         if err != nil {
             fmt.Printf("Error sending data to client: %s", err.Error())
@@ -49,39 +53,55 @@ func start_server(address string) {
     }
 }
 
-func receive_data(conn net.Conn) error {
-    fmt.Printf("Receiving data from client...\n")
+func receive_data(conn net.Conn) (map[string]string, error) {
+    //fmt.Printf("Receiving data from client...\n")
     buffer := make([]byte, 1024)
     read, err := conn.Read(buffer)
     if err != nil {
-        return err
+        return nil, err
     }
     data := string(buffer[:read])
-    fmt.Printf("Received data from client: %s", data)
-    return nil
+    lines := strings.Split(data, "\n")
+    headers := make(map[string]string)
+
+    headers["Status"] = lines[0]
+    for _, line := range lines[1:] {
+        split_index := strings.Index(line, ":")
+        if split_index != -1 {
+            headers[line[:split_index]] = line[split_index+1:]
+        }
+    }
+    return headers, nil
 }
 
 func send_data(conn net.Conn) error {
-    fmt.Printf("Sending data to client...\n")
+    //fmt.Printf("Sending data to client...\n")
 
-    html := "<!DOCTYPE html><html lang=\"ru\"><head><meta charset=\"UTF-8\"><title>Test</title></head><body>Ку, Андрюх!</body></html>"
+    //html := "<!DOCTYPE html><html lang=\"ru\"><head><meta charset=\"UTF-8\"><title>Test</title></head><body>Ку, Андрюх!</body></html>"
+    html := "Ку, Андрюх!"
+    response := create_response(html)
 
-    header := "HTTP/1.1 200 OK\n"
-    date := "Date: Wed, 11 Feb 2009 11:20:59 GMT\n"
-    server := "Server: Custom\n"
-    last_modified := "Last-Modified: Wed, 11 Feb 2009 11:20:59 GMT\n"
-    content_language := "Content-Language: ru\n"
-    content_type := "Content-Type: text/html; charset=utf-8\n"
-    content_length := "Content-Length: " + string(len(html)) + "\n"
-    connection := "Connection: close\n"
-
-
-    text := header + date + server + last_modified + content_language + content_type + content_length + connection + "\n" + html
-    _, err := conn.Write([]byte(text))
+    _, err := conn.Write([]byte(response))
     conn.Close()
     if err != nil {
         return err
     }
-    fmt.Printf("Sent data to client!\n")
+    //fmt.Printf("Sent data to client!\n")
     return nil
+}
+
+func create_response(html string) []byte {
+    status := "HTTP/1.1 200 OK\n"
+    date := "Date: " + time.Now().Format("Wed, 11 Feb 2009 11:20:59 GMT") + "\n"
+    server := "Server: Custom\n"
+    last_modified := "Last-Modified: " + time.Now().Format("Wed, 11 Feb 2009 11:20:59 GMT") + "\n"
+    content_language := "Content-Language: ru\n"
+    content_type := "Content-Type: text/html; charset=utf-8\n"
+    content_length := "Content-Length: " + string(len(html)) + "\n"
+    connection := "Connection: close\n"
+    headers := status + date + server + last_modified + content_language + content_type + content_length + connection
+
+    response := headers + "\n" + html
+
+    return []byte(response)
 }
